f30fb82f2f813d1658597f91618337512f88df10
alexisraelmtz <alex.israel.mtz@gmail.com>
2021-12-15 01:57:13 -0600

    Airflow sandbox env - ready w/local executor

--- astro/dags/validation_dag.py
+++ astro/dags/validation_dag.py
@@ -3,7 +3,7 @@ from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator, task
 from airflow.sensors.filesystem import FileSensor
 from airflow.operators.bash import BashOperator
-from airflow.operators.email import EmailOperator
+from airflow.operators.email_operator import EmailOperator
 from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta
 
@@ -21,14 +21,14 @@ default_args = {
     'retries': 2,
     'retry_delay': timedelta(minutes=5),
     'email_on_failure': True,
-    'email_on_retry': True,
+    'email_on_retry': False,
     'email': 'admin@astro.io',
     'owner': 'turingmachine',
     'on_failure_callback': report_failure
 }
 
 # Using a DAG context manager, you don't have to specify the dag property of each task
-with DAG('example',
+with DAG('email',
          start_date=datetime(2021, 12, 1),
          max_active_runs=3,
          schedule_interval=timedelta(minutes=30),
@@ -36,6 +36,6 @@ with DAG('example',
          catchup=False  # enable if you don't want historical dag runs to run
          ) as dag:
 
-    t0 = DummyOperator(
-        task_id='start'
+    start_task = DummyOperator(
+        task_id='start_task'
     )
